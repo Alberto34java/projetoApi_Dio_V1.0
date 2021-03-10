cba2de80ad9af699075b9fd1ba9cf95184258d76
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-09 21:13:24 -0300

    Alterado Arquivo de Controller

--- UniLacerdaApi/Controllers/FormacaoController.cs
+++ UniLacerdaApi/Controllers/FormacaoController.cs
@@ -1,7 +1,19 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using  System;
+//using Microsoft.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
 namespace UniLacerdaApi.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class FormacaoController : ControllerBase
     {
+
         
     }
 }
